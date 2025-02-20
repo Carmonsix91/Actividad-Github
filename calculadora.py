08785cfab4c2bfaacbd0501749c1ce7e031b4fe9
Jocelyn Lucia Amador Martinez <183007558+Joss1203@users.noreply.github.com>
2025-02-19 19:52:23 -0600

    multiplicación

---
+++ calculadora.py
@@ -0,0 +1,11 @@
+
+#definimos la función
+def multiplicacion(num1, num2):
+    return num1 * num2
+
+#opciones a elegir
+opcion = input("Ingresa una opción (1/2/3/4): ")
+
+
+elif opcion == '3':
+    print(num1, "*", num2, "=", multiplicacion(num1, num2))
