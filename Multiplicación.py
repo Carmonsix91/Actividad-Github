88b4b9de2a90abc0d568d0ef6a53876c8c985d9c
Jocelyn Lucia Amador Martinez <183007558+Joss1203@users.noreply.github.com>
2025-02-20 07:48:17 -0600

    Create Multiplicación.py

---
+++ Multiplicación.py
@@ -0,0 +1,9 @@
+def mulpiplicacion(a,b):
+  return a * b
+
+num1 = float(input("Primer número: "))
+num2 = float( input("Segundo número: "))
+
+resultado = multiplicacion (num1, num2)
+
+print("El resultado es: {resultado}")
